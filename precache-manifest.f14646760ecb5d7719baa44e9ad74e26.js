7a685a3f6d52223e31210b6268fe2b475a02f67d
Alex <CyberDasein>
2020-05-14 16:08:21 +0500

    Updates

---
+++ precache-manifest.f14646760ecb5d7719baa44e9ad74e26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783fb0e87e67c38fa5131ef8f5c797d2",
+    "url": "/BooksShop/index.html"
+  },
+  {
+    "revision": "1dfdfc7646b2b19af2ad",
+    "url": "/BooksShop/static/js/2.9ba96105.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BooksShop/static/js/2.9ba96105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d7e2559f6d4e4e08afd",
+    "url": "/BooksShop/static/js/main.f3993091.chunk.js"
+  },
+  {
+    "revision": "e038aed9cd39cb22e3f7",
+    "url": "/BooksShop/static/js/runtime-main.b26a05bb.js"
+  }
+]);
